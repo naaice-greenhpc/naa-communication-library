6fdd11e2134ebf317b3b57eb6543e35333609025
Hannes Signer <signer@uni-potsdam.de>
2024-11-13 10:57:00 +0100

    add single send option to naa_param_t and comment functions

--- lib/naaice_ap2.h
+++ lib/naaice_ap2.h
@@ -57,21 +57,31 @@ enum naa_error {
 };
 /* Structs *******************************************************************/
 
-// Struct to hold information about a single (input or output) parameter to
-// an NAA routine.
+/**
+ * @brief Struct to hold information about a single (input or output) parameter
+ *        to an NAA routine.
+ */ 
 typedef struct naa_param_t {
-  void *addr;
-  size_t size;
-  bool constant;
+  void *addr; // address to data region
+  size_t size; // size of data region
+  bool single_send; // send data only once (e.g. for configuration data)
 } naa_param_t;
 
-// Struct to hold information about a NAA session.
+
+/**
+ * @brief Struct to hold information about a NAA session.
+ * 
+ */
 typedef struct naa_handle {
-  unsigned int function_code;
-  struct naaice_communication_context *comm_ctx;
+  unsigned int function_code; // encodes the function to be executed on the NAA
+  struct naaice_communication_context *comm_ctx; // communication context for low level API
 } naa_handle;
 
-// Struct to hold status information about the NAA session.
+
+/**
+ * @brief Struct to hold status information about the NAA session.
+ * 
+ */
 typedef struct naa_status {
   enum naaice_communication_state state;
   enum naa_error naa_error;
@@ -81,17 +91,55 @@ typedef struct naa_status {
 
 /* Public Functions **********************************************************/
 
+/**
+ * @brief Configure memory regions and establish connection to remote.
+ * 
+ * @param function_code encodes calculation on peer
+ * @param input_params array of naa_param_t structs used as input regions
+ * @param input_amount number of input regions
+ * @param output_params array of naa_param_t structs used as output regions
+ * @param output_amount number of output regions
+ * @param handle communication handle created by naa_create
+ * @return int 0 if sucessful, -1 if not.
+ */
 int naa_create(unsigned int function_code, 
   naa_param_t *input_params, unsigned int input_amount,
   naa_param_t *output_params, unsigned int output_amount,
   naa_handle *handle);
 
+/**
+ * @brief Sends data to the peer.
+ * 
+ * @param handle communication handle created by naa_create
+ * @return int 0 if sucessful, -1 if not.
+ */
 int naa_invoke(naa_handle *handle);
 
+/**
+ * @brief Waits in non-blocking mode for a receive.
+ * 
+ * @param handle 
+ * @param flag 
+ * @param status 
+ * @return int 
+ */
 int naa_test(naa_handle *handle, bool *flag, naa_status *status);
 
+/**
+ * @brief Waits in blocking mode for a receive.
+ * 
+ * @param handle communication handle created by naa_create
+ * @param status 
+ * @return int 
+ */
 int naa_wait(naa_handle *handle, naa_status *status);
 
+/**
+ * @brief Terminates connection and cleans up the corresponding data structures.
+ * 
+ * @param handle communication handle created by naa_create
+ * @return int if sucessful, -1 if not.
+ */
 int naa_finalize(naa_handle *handle);
 
 #endif
