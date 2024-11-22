e400d1c3e1b2ce786f1db513dd58331b4e7187d2
Hannes Martin Signer <hannes.martin.signer@uni-potsdam.de>
2024-11-22 15:26:27 +0100

    check if compiled programs exist

--- ci-build.sh
+++ ci-build.sh
@@ -5,3 +5,13 @@ ROOT_DIR=$(pwd)
 mkdir bin && cd bin
 
 make -C .. release
+
+if [ ! -e naaice_client ]; then
+    echo "Error: naaice_client is missing"
+    exit 1  # Exit with a non-zero status to indicate failure
+fi
+
+if [ ! -e naaice_server ]; then
+    echo "Error: naaice_server is missing"
+    exit 1  # Exit with a non-zero status to indicate failure
+fi
