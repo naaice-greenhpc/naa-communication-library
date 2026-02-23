a41952875ed2b7d5c9a427fe64f24bda3f91a5be
Hannes Signer <signer@uni-potsdam.de>
2026-02-23 17:38:02 +0100

    disable logging using FetchContent_MakeAvailable and adapt examples for this feature

--- tests/naaice_server_measurement.c
+++ tests/naaice_server_measurement.c
@@ -92,8 +92,9 @@ static void cleanup_master_context(struct context *ctx) {
 }
 
 int main(int argc, __attribute__((unused)) char *argv[]) {
+#ifndef ULOG_BUILD_DISABLED
   ulog_output_level_set_all(LOG_LEVEL);
-
+#endif
   // Handle command line arguments.
   ulog_info("-- Handling Command Line Arguments --\n");
   if (argc != 1) {
